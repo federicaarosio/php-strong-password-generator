068e06cab2b8bc261b22945d33b0506feccba2ba
federicaarosio <arosio.fede@gmail.com>
2024-01-17 22:58:49 +0100

    added style to result.php

--- result.php
+++ result.php
@@ -9,12 +9,20 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Password generated</title>
+
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
 
 <body>
-    <p>
-        La tua password è: <?php echo $_SESSION['password']; ?>!
-    </p>
+    <div class="container">
+        <div class="row">
+            <div class=" col-12 alert alert-primary my-4" role="alert">
+                La tua password è: <strong><?php echo $_SESSION['password']; ?></strong>
+            </div>
+        </div>
+    </div>
 </body>
 
 </html>
